99cbedb73f92cc84efdac3aaacd218b5fc194d99
BrennerT <torben.brenner@googlemail.com>
2016-10-24 16:43:27 +0200

    changes of Sound and Vibration will now be written to Database

--- app/src/main/java/blau/team/remindme/SettingActivity.java
+++ app/src/main/java/blau/team/remindme/SettingActivity.java
@@ -3,27 +3,32 @@ package blau.team.remindme;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 
 import blau.team.remindme.db.Model;
+import blau.team.remindme.db.model.GPSPoint;
 import blau.team.remindme.db.model.Settings;
+import io.realm.Realm;
+import io.realm.RealmList;
 
 import static android.R.attr.mode;
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 import static android.media.CamcorderProfile.get;
+import static blau.team.remindme.R.id.sound;
 
 public class SettingActivity extends AppCompatActivity {
 
-    private Boolean vibration, sound;
+    private Settings settingInput;
     private Switch vb, sb;
     private Button gpsSetter1;
     private Button gpsSetter2;
     private Button gpsSetter3;
     private Button gpsSetter4;
     private Model model;
-    private Location[] gps;
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +36,10 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
         model = Model.getInstance();
+        realm = Realm.getDefaultInstance();
 
         vb = (Switch) findViewById(R.id.vibration);
-        sb = (Switch) findViewById(R.id.sound);
+        sb = (Switch) findViewById(sound);
         gpsSetter1 = (Button) findViewById(R.id.corner1);
         gpsSetter2 = (Button) findViewById(R.id.corner2);
         gpsSetter3 = (Button) findViewById(R.id.corner3);
@@ -41,64 +47,83 @@ public class SettingActivity extends AppCompatActivity {
 
         vb.setOnClickListener(vSwitchPressed);
         sb.setOnClickListener(sSwitchPressed);
-        gpsSetter1.setOnClickListener(gpsSetterPressed);
-        gpsSetter2.setOnClickListener(gpsSetterPressed);
-        gpsSetter3.setOnClickListener(gpsSetterPressed);
-        gpsSetter4.setOnClickListener(gpsSetterPressed);
+        gpsSetter1.setOnClickListener(gpsSetter1Pressed);
+        gpsSetter2.setOnClickListener(gpsSetter2Pressed);
+        gpsSetter3.setOnClickListener(gpsSetter3Pressed);
+        gpsSetter4.setOnClickListener(gpsSetter4Pressed);
 
-        //Laden der letzten Einstellungen
-        if(model.getSettings() != null){
-            vb.setEnabled(Boolean.valueOf(model.getSettings().isVibration()));
-            sb.setEnabled(Boolean.valueOf(model.getSettings().isSound()));
-            for (int i =0; i < 4; i++){
-                gps[i].setLatitude(Double.valueOf(model.getSettings().getCorners().get(i+1).getLatitude()));
-                gps[i].setLongitude(Double.valueOf(model.getSettings().getCorners().get(i+1).getLongitude()));
+        // loads settings from database
+        settingInput = model.getSettings();
+
+        // if no settings are found make default settings
+        if(settingInput == null) {
+            Log.d("| Entering default","");
+            RealmList<GPSPoint> corners = new RealmList<>();
+            for (int i = 0; i <= 3; i++) {
+                corners.add(new GPSPoint(0.0, 0.0));
             }
+            settingInput = new Settings(false, false, corners);
         }
+
+        Log.d("| settingInput:",settingInput.toString());
+
+        // Update GUI
+        Log.d("| Value sound Property",String.valueOf(settingInput.isSound()));
+        vb.setChecked(settingInput.isVibration());
+        sb.setChecked(settingInput.isSound());
+
+        gpsSetter1.setText(String.valueOf(settingInput.getCorners().get(0).getLatitude()) + "\n" + String.valueOf(settingInput.getCorners().get(0).getLongitude()));
+        gpsSetter2.setText(String.valueOf(settingInput.getCorners().get(1).getLatitude()) + "\n" + String.valueOf(settingInput.getCorners().get(1).getLongitude()));
+        gpsSetter3.setText(String.valueOf(settingInput.getCorners().get(2).getLatitude()) + "\n" + String.valueOf(settingInput.getCorners().get(2).getLongitude()));
+        gpsSetter4.setText(String.valueOf(settingInput.getCorners().get(3).getLatitude()) + "\n" + String.valueOf(settingInput.getCorners().get(3).getLongitude()));
+
     }
 
     View.OnClickListener vSwitchPressed = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            SettingActivity.this.setVibration(!getVibration());
+            realm.beginTransaction();
+            settingInput.setVibration(!settingInput.isVibration());
+            realm.commitTransaction();
         }
     };
 
     View.OnClickListener sSwitchPressed = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            SettingActivity.this.setSound(!getSound());
+            realm.beginTransaction();
+            settingInput.setSound(!settingInput.isSound());
+            realm.commitTransaction();
         }
     };
 
-    View.OnClickListener gpsSetterPressed = new View.OnClickListener() {
+    View.OnClickListener gpsSetter1Pressed = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+
         }
     };
 
-    public Location[] getGps() {
-        return gps;
-    }
+    View.OnClickListener gpsSetter2Pressed = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-    public void setGps(Location[] gps) {
-        this.gps = gps;
-    }
-    public Boolean getVibration() {
-        return vibration;
-    }
+        }
+    };
 
-    public void setVibration(Boolean vibration) {
-        this.vibration = vibration;
-    }
+    View.OnClickListener gpsSetter3Pressed = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
 
-    public Boolean getSound() {
-        return sound;
-    }
+        }
+    };
 
-    public void setSound(Boolean sound) {
-        this.sound = sound;
-    }
+    View.OnClickListener gpsSetter4Pressed = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
 
     public Model getModel() {
         return model;
@@ -113,6 +138,6 @@ public class SettingActivity extends AppCompatActivity {
     {
         super.onDestroy();
         //Updaten der DB
-        //model.setSettings(new Settings(this.getSound(),this.getVibration(),this.getGps()));
+        model.setSettings(settingInput);
     }
 }
